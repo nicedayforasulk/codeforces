605ab205686a65ab77fec4404f1ae32f582b150d
nicedayforasulk <vaishalib12@gmail.com>
2021-06-03 21:22:48 -0700

    theatre square

--- src/TheatreSquare.java
+++ src/TheatreSquare.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class TheatreSquare {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        long length = scanner.nextLong();
-        long width = scanner.nextLong();
-        double tileLength = scanner.nextDouble();
-        double numRequiredTiles =  (Math.ceil(length/tileLength) * Math.ceil(width/tileLength));
-        System.out.println((long) (numRequiredTiles));
+        int length = scanner.nextInt();
+        int width = scanner.nextInt();
+        int tileLength = scanner.nextInt();
+
+        int numTilesToCoverLength = computeTilesRequired(length, tileLength);
+        int numTilesToCoverWidth = computeTilesRequired(width, tileLength);
+
+        System.out.println((long)numTilesToCoverLength * numTilesToCoverWidth);
+    }
+
+    private static int computeTilesRequired(int toCover, int tileLength) {
+        int min = toCover/tileLength;
+        if(toCover % tileLength == 0){
+            return min;
+        }
+        return min + 1;
     }
 }
