919f27a4c42dbff4dcb43b70d2dbf6afd5c6537c
nicedayforasulk <vaishalib12@gmail.com>
2021-06-06 11:17:17 -0700

    domino piling

---
+++ src/DominoPiling.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int M = scanner.nextInt();
+        int N = scanner.nextInt();
+        System.out.println(M*N/2);
+    }
+
+}
